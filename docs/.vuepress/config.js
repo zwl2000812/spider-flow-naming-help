340e0709ffd714b02fd2cf1d278a7841d58f24ba
朱旺龙 <zwl@baomutech.com>
2022-06-23 16:36:23 +0800

    添加回到顶部插件，添加package-lock.json，需注意clone仓库后需要npm install -D vuepress npm install -D @vuepress/plugin-back-to-top  npm install -D @vuepress/plugin-active-header-links

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,8 +1,9 @@
 module.exports = {
-  title: 'Spider-Flow命名规范',
-  description: '规范spider-flow爬虫模板的命名与zabbix互通',
-  themeConfig: {
-    sidebar: 'auto',
-    displayAllHeaders: true // 默认值：false
-  }
+    title: 'Spider-Flow命名规范',
+    description: '规范spider-flow爬虫模板的命名与zabbix互通',
+    themeConfig: {
+        sidebar: 'auto',
+        displayAllHeaders: true // 默认值：false
+    },
+  plugins: ['@vuepress/active-header-links','@vuepress/back-to-top']
 }
